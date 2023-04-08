3fa0eb008c5dbf0de1bf383d5d21ae254f125751
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-08 21:03:11 +0630

    slices - use arrays under the hood

--- main.go
+++ main.go
@@ -7,4 +7,11 @@ func main() {
 	names := [4]string{"HEE", "JEE", "MEE", "KEE"}
 	fmt.Println(ages, len(ages))
 	fmt.Println(names, len(names))
+
+	// slices (use arrays under the hood)
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 58)
+
+	fmt.Println(scores, len(scores))
 }

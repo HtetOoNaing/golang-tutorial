14c3422cee90c160f97828682fff4dd5685d4c47
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-08 21:09:01 +0630

    slice ranges

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 	var scores = []int{100, 50, 60}
 	scores[2] = 25
 	scores = append(scores, 58)
-
 	fmt.Println(scores, len(scores))
+
+	// slice ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+	fmt.Println(rangeOne, len(rangeOne))
+	fmt.Println(rangeTwo, len(rangeTwo))
+	fmt.Println(rangeThree, len(rangeThree))
+	rangeOne = append(rangeOne, "IEE")
+	fmt.Println(rangeOne, len(rangeOne))
 }

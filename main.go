c184a249e95dbf530f8b3ae92d778e9385616eb6
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-08 10:17:12 +0630

    integer variables

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var nameOne string = "Htet"
-	var nameTwo = "Oo"
-	var nameThree string
-	fmt.Println(nameOne, nameTwo, nameThree)
-	nameOne = "Htet1"
-	nameTwo = "Oo1"
-	nameThree = "Naing1"
-	fmt.Println(nameOne, nameTwo, nameThree)
-	nameFour := "luios"
-	fmt.Println(nameFour)
+	// strings
+	// var nameOne string = "Htet"
+	// var nameTwo = "Oo"
+	// var nameThree string
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameOne = "Htet1"
+	// nameTwo = "Oo1"
+	// nameThree = "Naing1"
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameFour := "luios"
+	// fmt.Println(nameFour)
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 21
+	ageThree := 22
+	fmt.Println(ageOne, ageTwo, ageThree)
 }

69b2b03d90ca37cd907094f813725ba40dded2ed
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-08 23:29:49 +0630

    sort ints and search ints

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-	sort.Ints(ages)
+	// sort.Ints(ages)
 	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 900)
+	fmt.Println(index)
 }

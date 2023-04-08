86707391ef3b9f3a100d92f63f54f8b8b00d6551
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-08 22:20:36 +0630

    strings methods

--- main.go
+++ main.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	greeting := "hello there my friends!"
 	fmt.Println(strings.Contains(greeting, "helloo"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "ll"))
+	fmt.Println(strings.Split(greeting, " "))
 }

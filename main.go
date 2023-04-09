87ba0f7912a13a9dd2df213f54a862a177e16790
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-09 11:42:14 +0630

    function return multiple values

--- main.go
+++ main.go
@@ -2,35 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreeting(n string) {
-	fmt.Printf("Good morning %v \n", n)
-}
+func circleArea(s string) (string, string) {
+	upper := strings.ToUpper(s)
+	names := strings.Split(upper, " ")
+	var initials []string
 
-func sayBye(n string) {
-	fmt.Printf("Goodbye %v \n", n)
-}
-
-func cycleNames(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
+	for _, v := range names {
+		initials = append(initials, v[:1])
 	}
-}
 
-func circleArea(r float64) float64 {
-	return math.Pi * r * r
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+	return initials[0], "_"
+
 }
 
 func main() {
-	// sayGreeting("mario")
-	// sayGreeting("luigi")
-	// sayBye("mario")
-	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
-	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
-	a1 := circleArea(10.5)
-	a2 := circleArea(15)
-	fmt.Println(a1, a2)
-	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+	fn1, sn1 := circleArea("dj soda")
+	fmt.Println(fn1, sn1)
 }

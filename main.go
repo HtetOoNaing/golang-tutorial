2b02e7512819f29081a6bddcbad3679b6d2e39f9
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-09 11:15:45 +0630

    loop with range over array and continue break

--- main.go
+++ main.go
@@ -16,4 +16,17 @@ func main() {
 	} else {
 		fmt.Println("age is greater than 40")
 	}
+
+	names := []string{"maria", "luigi", "yoshi", "peach", "bowser"}
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 }

b199bdb8c87e195e71755eb7f76b58f8d7e83886
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-09 10:39:07 +0630

    boolean comparison

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("x value is", x)
-	// 	x++
-	// }
-
-	// for x := 0; x < 5; x++ {
-	// 	fmt.Println("x value is", x)
-	// }
-
-	names := []string{"mario", "luigi", "yoshi", "peach"}
-	// for x := 0; x < len(names); x++ {
-	// 	fmt.Println(x, "is", names[x])
-	// }
-	// for index, value := range names {
-	// 	fmt.Printf("The value at index %v is %v \n", index, value)
-	// }
-
-	for _, value := range names {
-		fmt.Printf("The value is %v \n", value)
-	}
+	age := 45
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
 }

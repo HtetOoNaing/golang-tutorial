777ecf9f984a8fea2987f78d8965bb468cf4aad6
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-10 08:40:31 +0630

    create getInput func and use to get user input

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 
 var score = 99.5
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Create a new bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
 	return b

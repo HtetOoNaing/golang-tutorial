a7b755dbed9a99936e88ecadbbe9eca8aaa40d3f
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-10 08:21:21 +0630

    create new bill with user input name

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 var score = 99.5
 
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Create a new bill name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+	return b
+}
+
 func main() {
-	myBill := newBill("mario's bil")
-	myBill.updateTip(10.5)
-	myBill.addItem("chili", 44)
+	myBill := createBill()
 	fmt.Println(myBill)
 	fmt.Println(myBill.format())
 }

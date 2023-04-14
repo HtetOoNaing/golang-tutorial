62c397574ce967dd4c5ffb3532ca11de64d9f282
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-04-14 22:44:18 +0630

    switch for options

--- main.go
+++ main.go
@@ -23,8 +23,29 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+	case "s":
+		fmt.Println("You chose s")
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+		fmt.Println(tip)
+	default:
+		fmt.Println("That is not a valid option...")
+		promptOptions(b)
+	}
+}
+
 func main() {
 	myBill := createBill()
-	fmt.Println(myBill)
-	fmt.Println(myBill.format())
+	promptOptions(myBill)
+	// fmt.Println(myBill)
+	// fmt.Println(myBill.format())
 }
